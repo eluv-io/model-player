d4ce37f182c1df8a62977539d40abf7553d3926e
elv-joeA <joe.anakata@eluv.io>
2025-04-23 00:03:06 +0000

    add non-readonly cache for test; this model needs it

--- test.sh
+++ test.sh
@@ -9,7 +9,6 @@ mkdir test_output
 [ "$ELV_MODEL_TEST_GPU_TO_USE" != "" ] || ELV_MODEL_TEST_GPU_TO_USE=0
 
 mkdir -p .cache
-mkdir -p .cache-ro
 
 ##--volume=$(pwd)/models:/elv/models:ro podman run --rm
 #podman run nvidia.com/gpu=$ELV_MODEL_TEST_GPU_TO_USE player test/*.jpg
@@ -17,7 +16,7 @@ mkdir -p .cache-ro
 #--volume=$(pwd)/test_output:/elv/tags
 #--volume=$(pwd)/.cache:/root/.cache --network host --device
 
-podman run --rm --volume=$(pwd)/test:/elv/test:ro --volume=$(pwd)/test_output:/elv/tags --volume=$(pwd)/.cache-ro:/root/.cache-ro:ro --network host --device nvidia.com/gpu=$ELV_MODEL_TEST_GPU_TO_USE player test/*.JPG
+podman run --rm --volume=$(pwd)/test:/elv/test:ro --volume=$(pwd)/test_output:/elv/tags --volume=$(pwd)/.cache:/root/.cache --network host --device nvidia.com/gpu=$ELV_MODEL_TEST_GPU_TO_USE player test/*.JPG
 
 ex=$?
 
